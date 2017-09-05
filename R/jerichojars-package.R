bf59d6983146b70df283e85f24d35839db0b9a5e
boB Rudis <bob@rud.is>
2017-09-04 21:18:34 -0400

    initial commit

---
+++ R/jerichojars-package.R
@@ -0,0 +1,12 @@
+#' Java Archive Wrapper Supporting the 'jericho' Package
+#'
+#' Contents of the 'Jericho HTML Parser' Java archive by
+#' Martin Jericho <http://jericho.htmlparser.net/docs/index.html> provided to
+#' support functions in the 'jericho' package.
+#'
+#' @md
+#' @name jerichojars
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import rJava
+NULL
